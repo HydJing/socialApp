bc490b1b710ee5c5a2a8f46f60bdc29ecf0c7c52
Wei Jing <jaychou123103@hotmail.com>
2020-03-01 00:17:27 +1300

    Add DTO(Data Transfer Object)

--- socialApp.API/Controllers/AuthController.cs
+++ socialApp.API/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using socialApp.API.Data;
+using socialApp.API.Dtos;
 using socialApp.API.Models;
 
 namespace socialApp.API.Controllers
@@ -19,21 +20,21 @@ namespace socialApp.API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> Register(string username, string password)
+        public async Task<IActionResult> Register(UserRegisterDto userRegisterDto)
         {
-            username = username.ToLower();
+            userRegisterDto.Username = userRegisterDto.Username.ToLower();
 
-            if (await _repo.userExists(username))
+            if (await _repo.userExists(userRegisterDto.Username))
             {
                 return BadRequest("Username already taken");
             }
 
             var userToCreate = new User
             {
-                Username = username
-            }
+                Username = userRegisterDto.Username
+            };
 
-            var createdUser = await _repo.Register(userToCreate, password);
+            var createdUser = await _repo.Register(userToCreate, userRegisterDto.Password);
 
             return StatusCode(201);
         }
---
+++ socialApp.API/Dtos/UserRegisterDto.cs
@@ -0,0 +1,8 @@
+namespace socialApp.API.Dtos
+{
+    public class UserRegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}

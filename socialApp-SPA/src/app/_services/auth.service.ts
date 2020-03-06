bd5a24f8588c7a05b939aaa6c49ec7ed179bdc99
Wei Jing <jaychou123103@hotmail.com>
2020-03-07 01:02:03 +1300

    Add decode token by jwt

--- socialApp-SPA/src/app/_services/auth.service.ts
+++ socialApp-SPA/src/app/_services/auth.service.ts
@@ -9,15 +9,20 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 export class AuthService {
   baseURL = 'http://localhost:5000/api/auth/';
   jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
 constructor(private http: HttpClient) { }
 
 login(model: any) {
-  return this.http.post(this.baseURL + 'login', model).pipe(
+  return this.http
+  .post(this.baseURL + 'login', model)
+  .pipe(
     map((response: any) => {
       const user = response;
       if (user) {
         localStorage.setItem('token', user.token);
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        console.log(this.decodedToken);
       }
     })
   );

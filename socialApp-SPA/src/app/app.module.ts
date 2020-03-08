829173152d43c2ea205b231c7502099a21278074
Wei Jing <jaychou123103@hotmail.com>
2020-03-09 00:23:29 +1300

    Add Route to Angular

--- socialApp-SPA/src/app/app.module.ts
+++ socialApp-SPA/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -12,6 +13,11 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
+
 
 
 @NgModule({
@@ -19,7 +25,10 @@ import { AlertifyService } from './_services/alertify.service';
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
@@ -27,11 +36,12 @@ import { AlertifyService } from './_services/alertify.service';
       FormsModule,
       BsDropdownModule.forRoot(),
       BrowserAnimationsModule,
+      RouterModule.forRoot(appRoutes),
    ],
    providers: [
       AuthService,
       ErrorInterceptorProvider,
-      AlertifyService,
+      AlertifyService
    ],
    bootstrap: [
       AppComponent

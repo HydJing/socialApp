53c2c52f358ef2f0a83710cf1a4f9c938857a90b
Wei Jing <jaychou123103@hotmail.com>
2020-03-19 00:39:02 +1300

    Add photo gallery by ngx gallery

--- socialApp-SPA/src/app/app.module.ts
+++ socialApp-SPA/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -6,6 +6,7 @@ import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 
 import { AppComponent } from './app.component';
@@ -29,6 +30,12 @@ export function tokenGetter() {
    return localStorage.getItem('token');
 }
 
+export class CustomHammerConfig extends HammerGestureConfig {
+   overrides = {
+     pinch: { enable: false },
+     rotate: { enable: false }
+   };
+ }
 
 @NgModule({
    declarations: [
@@ -50,6 +57,7 @@ export function tokenGetter() {
       TabsModule.forRoot(),
       BrowserAnimationsModule,
       RouterModule.forRoot(appRoutes),
+      NgxGalleryModule,
       JwtModule.forRoot({
          config: {
             tokenGetter: tokenGetter,
@@ -64,6 +72,7 @@ export function tokenGetter() {
       AlertifyService,
       MemberDetailResolver,
       MemberListResolver,
+      { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig },
    ],
    bootstrap: [
       AppComponent
--- socialApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ socialApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -3,6 +3,7 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
 import { UserService } from 'src/app/_services/user.service';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/user';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from '@kolkov/ngx-gallery';
 
 @Component({
   selector: 'app-member-detail',
@@ -12,6 +13,8 @@ import { User } from 'src/app/_models/user';
 export class MemberDetailComponent implements OnInit {
 
   user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(private userSerice: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
@@ -20,6 +23,34 @@ export class MemberDetailComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.user = data.user;
     });
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+
+    this.galleryImages = this.getImages();
+  }
+
+  getImages() {
+    const imageUrls = [];
+    for (const photo of this.user.photos) {
+      console.log(photo)
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url,
+        description: photo.description,
+      });
+    }
+
+    return imageUrls;
   }
 
   // loadUser() {

c7f010ea6ebd64529ffbe6ac3cd8ca09ce2cd44d
Wei Jing <jaychou123103@hotmail.com>
2020-04-02 00:29:54 +1300

    Add pagination in API

---
+++ socialApp.API/Helpers/UserParams.cs
@@ -0,0 +1,15 @@
+namespace socialApp.API.Helpers
+{
+    public class UserParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize = 10;
+        public int MyProperty
+        {
+            get { return PageSize; }
+            set { PageSize = (value > MaxPageSize) ? MaxPageSize : value; }
+        }
+        
+    }
+}

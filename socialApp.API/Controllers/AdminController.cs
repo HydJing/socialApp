0196854bdc637913d63e28601428659f2ed1cd0c
Wei Jing <jaychou123103@hotmail.com>
2020-04-29 00:19:20 +1200

    Add photo management part 2

--- socialApp.API/Controllers/AdminController.cs
+++ socialApp.API/Controllers/AdminController.cs
@@ -1,11 +1,15 @@
 using System.Linq;
 using System.Threading.Tasks;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using socialApp.API.Data;
 using socialApp.API.Dtos;
+using socialApp.API.Helpers;
 using socialApp.API.Models;
 
 namespace socialApp.API.Controllers
@@ -16,16 +20,25 @@ namespace socialApp.API.Controllers
     {
         private readonly DataContext _context;
         private readonly UserManager<User> _userManager;
+        private readonly IOptions<CloudinarySettings> _cloudinaryConfig;
         private Cloudinary _cloudinary;
 
         public AdminController(
             DataContext context, 
             UserManager<User> userManager, 
-            IOptions<CloudinarySettings> cloudinarySettings)
+            IOptions<CloudinarySettings> cloudinaryConfig)
         {
             _userManager = userManager;
+            _cloudinaryConfig = cloudinaryConfig;
             _context = context;
-            _cloudinary = cloudinarySettings;
+            
+            Account acc = new Account(
+                _cloudinaryConfig.Value.CloudName,
+                _cloudinaryConfig.Value.ApiKey,
+                _cloudinaryConfig.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(acc);
         }
 
         [Authorize(Policy = "RequireAdminRole")]
@@ -79,7 +92,7 @@ namespace socialApp.API.Controllers
 
         [Authorize(Policy = "ModeratePhotoRole")]
         [HttpGet("photoForModeration")]
-        public async IActionResult GetPhotosForModeration()
+        public async Task<IActionResult> GetPhotosForModeration()
         {
             var photos = await _context.Photos
                 .Include(u => u.User)
@@ -92,7 +105,57 @@ namespace socialApp.API.Controllers
                     IsApproved = u.IsApproved
                 }).ToListAsync();
             
-            return OK(photos);
+            return Ok(photos);
+        }
+
+        [Authorize(Policy = "ModeratePhotoRole")]
+        [HttpPost("approvePhoto/{photoId}")]
+        public async Task<IActionResult> ApprovePhoto(int photoId)
+        {
+            var photo = await _context.Photos
+                .IgnoreQueryFilters()
+                .FirstOrDefaultAsync(p => p.Id == photoId);
+            
+            photo.IsApproved = true;
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [Authorize(Policy = "ModeratePhotoRole")]
+        [HttpPost("rejectPhoto/{photoId}")]
+        public async Task<IActionResult> RejectPhoto(int photoId)
+        {
+            var photo = await _context.Photos
+                .IgnoreQueryFilters()
+                .FirstOrDefaultAsync(p => p.Id == photoId);
+            
+            if (photo.IsMain)
+            {
+                return BadRequest("You cannot reject the main photo.");
+            }
+
+            if (photo.PublicId != null)
+            {
+                var deleteParams = new DeletionParams(photo.PublicId);
+
+                var result = _cloudinary.Destroy(deleteParams);
+
+                if (result.Result == "ok") 
+                {
+                    _context.Photos.Remove(photo);
+                }
+            }
+
+            if (photo.PublicId == null) 
+            {
+                _context.Photos.Remove(photo);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
         }
     }
 }
